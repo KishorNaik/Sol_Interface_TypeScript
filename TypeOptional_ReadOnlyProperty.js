36310ab8ef8afc27b29b59a6bb0624108d692149
KishorNaik <kishor.naik011.net@gmail.com>
2020-03-31 13:39:22 +0530

    code commit

---
+++ TypeOptional_ReadOnlyProperty.js
@@ -0,0 +1,9 @@
+var userObj = {
+    fullName: "Kishor Naik",
+    //age:35, // Optional Property
+    salary: 1
+};
+console.log(userObj.fullName);
+console.log(userObj.age);
+console.log(userObj.salary);
+//userObj.salary = 10; // Cannot assign value in salary property becuase it is readonly
---
+++ TypeOptional_ReadOnlyProperty.ts
@@ -0,0 +1,18 @@
+interface IUsers {
+    fullName: string,
+    age?: number,
+    readonly salary: number
+}
+
+let userObj: IUsers = {
+    fullName: "Kishor Naik",
+    //age:35, // Optional Property
+    salary: 1
+};
+
+console.log(userObj.fullName);
+console.log(userObj.age);
+console.log(userObj.salary);
+
+//userObj.salary = 10; // Cannot assign value in salary property becuase it is readonly
+
